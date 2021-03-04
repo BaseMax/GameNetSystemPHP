9bbdbb34e0f0ba2f858694a3ff873af79363aef6
Max Base <MaxBaseCode@Gmail.Com>
2021-03-04 22:45:25 +0330

    Support LIVE

--- script.js
+++ script.js
@@ -83,7 +83,7 @@ function sendRequest(link, params, callback) {
   var xhttp = new XMLHttpRequest();
   xhttp.onreadystatechange = function() {
     if(this.readyState == 4 && this.status == 200) {
-      console.log("output response: " + this.responseText);
+    //   console.log("output response: " + this.responseText);
       callback(this.responseText);
     }
   };
@@ -94,7 +94,7 @@ function sendRequest(link, params, callback) {
     res_param+=key + "=" + params[key]+"&"; 
   }
   res_param = res_param.rtrim();
-  console.log(link, res_param);
+//   console.log(link, res_param);
   xhttp.send(res_param);
 }
 
@@ -178,9 +178,51 @@ function sendAddFood(planID, planIndex, planTabID, food, count) {
   })
 }
 
-function openChangeTime(elm, orderID) {
-  let editBox = elm.parentElement.querySelector("#edit-manualy");
-  editBox.style.display = "block";
+function openChangeTime(type, elm, orderID, default_value) {
+//   let editBox = elm.parentElement.querySelector("#edit-manualy");
+//   editBox.style.display = "block";
+    if(type === 1) {
+        let min = prompt("تعداد دقیقه تا کنون:", default_value);
+        if(min === undefined || min === null) {
+            return;
+        }
+        min = parseInt(min);
+        
+        if(min < 0) {
+            min = 0;
+        }
+
+        let field = elm.parentElement.querySelector("input[name=time]");
+        field.value = min;
+        
+        /////////////////////////
+        
+        let has_timer = confirm("آیا زمان معکوس اعمال شود؟");
+        console.log(has_timer);
+        
+        field = elm.parentElement.querySelector("input[name=timer]");
+        field.value = (has_timer === true ? 1 : 0);
+
+        changeTime(elm, orderID);
+    }
+    else {
+        let daste = prompt("تعداد دسته:", default_value);
+        if(daste === undefined || daste === null) {
+            return;
+        }
+        daste = parseInt(daste);
+        
+        if(daste < 1) {
+            daste=1;
+        }
+        else if(daste > 4) {
+            daste = 4;
+        }
+        
+        let field = elm.parentElement.querySelector("input[name=daste]");
+        field.value = daste;
+        changeTime(elm, orderID);
+    }
 }
 
 function changeTime(elm, orderID) {
@@ -189,14 +231,132 @@ function changeTime(elm, orderID) {
 
   let time = elm.parentElement.querySelector("input[name=time]");
   console.log(time);
-  let daste = elm.parentElement.querySelector("select[name=daste]")
+  let daste = elm.parentElement.querySelector("input[name=daste]")
   console.log(daste);
+  let has_timer = elm.parentElement.querySelector("input[name=timer]")
+  console.log(has_timer);
   // alert(time.value);
   sendRequest("api/change-time-manually.php", {
     orderID: orderID,
     time: time.value,
-    daste: daste.options[daste.selectedIndex].value,
+    timer: has_timer.value,
+    // daste: daste.options[daste.selectedIndex].value,
+    daste: daste.value,
   }, function(res) {
     alert(res);
   })
 }
+
+
+/* LIVE VARIABLES */
+var LIVE = "";
+var table_list;
+var systems;
+
+/* LIVE */
+function live_table(obj) {
+    let html = ``
+    for(let plan of LIVE.table) {
+        let box = `<div class="rows-${plan.count}">`
+        for(let item of plan.items) {
+            let block = `<div id="btn-system-${item.id}" class="col ${item.color}" onclick="openCity(event, 'system-${item.id}')">
+			    ${item.id}
+			    &nbsp;
+			    ${item.family}
+			<hr>
+			<b>قیمت: </b>
+			<br>
+			<label>
+			    ${item.price}
+			</label>
+			<hr>
+			<b>زمان: </b>
+			<br>
+			<label>
+			    ${
+			        item.timer === 1 ? item.timer_left : item.time
+			    }
+			</label>
+			<hr>
+			<b>دسته: </b>
+			<br>
+			<label>
+			    ${item.daste}
+			</label>
+		</div>`
+            box += block
+        }
+        html += box
+    }
+    table_list.innerHTML = html
+}
+
+function live_sections(obj) {
+    let i = 1;
+    for(let section of LIVE.section) {
+        let block = systems[i-1];
+        console.log(block);
+        
+        let price = block.querySelector(".total-price");
+        price.innerText = section.price;
+        console.log(price);
+        console.log(section.price);
+
+        let payment = block.querySelector(".total-price-to-payment");
+        payment.innerText = section.price_payment;
+        console.log(payment);
+        console.log(section.price_payment);
+
+        let html = ``;
+        for(let item of section.table) {
+            let code = `<tr>
+                <td>
+                    ${item.daste}
+                </td>
+                <td>
+                    ${item.date}
+                </td>
+                <td>
+                    ${item.time}
+                    ${
+                        (item.timer === 1) ? "(" + item.timer_left + ")" : ""
+                    }
+                    <input name="daste" type="hidden" value="${item.daste}">
+                    <input name="time" type="hidden" value="${item.time_min}">
+                    <input name="timer" type="hidden" value="${item.timer}">
+                    <button onclick="openChangeTime(1, this, ${item.id}, ${item.time_min});">تغییر زمان</button>
+                    <button onclick="openChangeTime(2, this, ${item.id}, ${item.daste});">تغییر دسته</button>
+                </td>
+                <td>
+                    ${item.price} تومان
+                </td>
+            </tr>`
+            html += code
+        }
+        let table = block.querySelector(".table_content_rows");
+        table.innerHTML = html;
+
+        i++;
+    }
+}
+
+function live() {
+    sendRequest("api/live/home-table.php", {}, function(res) {
+        // console.log("response", res);
+        if(res === undefined || res === "") {
+            return;
+        }
+        let obj = JSON.parse(res);
+        LIVE = obj;
+
+        live_table(obj);
+        live_sections(obj);
+    });
+}
+
+window.addEventListener('load', function() {
+    table_list = document.querySelector("#new-customer");
+    systems = document.querySelectorAll(".tabcontent-system");
+    live();
+    setInterval(live, 5000);
+});
